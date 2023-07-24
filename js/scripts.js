45672c8dc4931d324834fc8eab1deb5e7698f023
dzuba110729 <dzyuba1107@outlook.com>
2023-07-24 11:43:36 +0300

    new file

--- js/scripts.js
+++ js/scripts.js
@@ -255,6 +255,31 @@ function initRestabook() {
         });
         resize2 = false;
     }
+
+    const tabs = document.querySelectorAll('.tab-item');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    function changeTab(tabIndex) {
+        // Удаление класса "current" у всех вкладок
+        tabs.forEach(tab => tab.classList.remove('current'));
+
+        // Добавление класса "current" для выбранной вкладки
+        tabs[tabIndex].classList.add('current');
+
+        // Скрытие всех содержимых вкладок
+        tabContents.forEach(content => content.style.display = 'none');
+
+        // Отображение содержимого выбранной вкладки
+        tabContents[tabIndex].style.display = 'block';
+    }
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            changeTab(index);
+        });
+    });
+
+
     //первый слайдер
     const swiper = document.querySelectorAll('.fa-swiper');
     const prevSwiper = document.querySelector('.ec-button-prev');
